69c9a8e0aa38451d3c036642fc3a8de50fa148d9
Muhammad Ihsan <ihsan@embreo.global>
2024-01-22 06:41:34 +0700

    improvement query

--- controllers/party.controller.go
+++ controllers/party.controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/automa8e_clone/constants"
 	"github.com/automa8e_clone/db"
@@ -14,54 +15,60 @@ import (
 	"gorm.io/gorm/clause"
 )
 
-
+type PartyListElement struct {
+	ID				string			`json:"id"`
+	Name			string			`json:"name"`
+	Country			string			`json:"country"`
+	AddressLine1	string			`json:"address_line_1"`
+	CreatedAt		time.Time		`json:"created_at"`
+}
 type ReturnParties struct {
-	Data			[]models.Party 					`json:"data"`
-	Pagination		types.PaginationResponse		`json:"pagination"`
+	Data       []PartyListElement `json:"data"`
+	Pagination types.PaginationResponse `json:"pagination"`
 }
 
 type BodyPostParty struct {
-	CompanyName		string		`validate:"required" json:"company_name"`
-	AddressLine1	string		`validate:"required" json:"address_line_1"`
-	AddressLine2	string		`json:"address_line_2"`
-	AddressLine3	string		`json:"address_line_3"`
-	PostalCode		string		`json:"postal_code"`
-	CountryId		string		`json:"country_id"`
+	CompanyName  string `validate:"required" json:"company_name"`
+	AddressLine1 string `validate:"required" json:"address_line_1"`
+	AddressLine2 string `json:"address_line_2"`
+	AddressLine3 string `json:"address_line_3"`
+	PostalCode   string `json:"postal_code"`
+	CountryId    string `json:"country_id"`
 }
 
 func GetParties(c *gin.Context) {
 
-	var data []models.UserPartyPermission
-	var count int64;
+	var data []PartyListElement
+	var count int64
 
 	userCtx, _ := c.Get("user")
 	user := userCtx.(jwt.MapClaims)
 
-	paginationCtx ,_ := c.Get("pagination")
+	paginationCtx, _ := c.Get("pagination")
 	pagination := paginationCtx.(types.PaginationQuery)
 
-	// Find Data
-	db.PSQL.Offset(pagination.Offset).Limit(pagination.PageSize).Where("user_id = ?", user["sub"]).Preload("Party").Preload("User").Preload("Party.Country").Find(&data)
+	db.PSQL.
+	Table("user_party_permissions").
+	Offset(pagination.Offset).
+	Limit(pagination.PageSize).
+	Joins("JOIN parties ON parties.id = user_party_permissions.party_id").
+	Joins("JOIN countries ON parties.country_id = countries.id").
+	Select("parties.id, parties.name, parties.created_at, parties. postal_code, parties.address_line1, countries.name AS country").
+	Order("parties.created_at desc").
+	Where("user_party_permissions.user_id = ?", user["sub"]).
+	Find(&data).
+	Count(&count)
 
 	// Find Total Data
-	db.PSQL.Model(&models.UserPartyPermission{}).Where("user_id = ?", user["sub"]).Count(&count)
-
-	var formattedData []models.Party = []models.Party{}
-
-	for _, item := range data {
-		var singleData models.Party = models.Party{}
-		singleData = item.Party
-		add :=append(formattedData, singleData)
-		formattedData = add;
-	}
+	// db.PSQL.Model(&models.UserPartyPermission{}).Where("user_id = ?", user["sub"]).Count(&count)
 
 	response := ReturnParties{
-		Data: formattedData,
+		Data: data,
 		Pagination: types.PaginationResponse{
-			TotalData: count,
+			TotalData:   count,
 			CurrentPage: pagination.Page,
-			TotalPage: int(helpers.FindTotalPage(count, pagination.PageSize)),
-			PageSize: pagination.PageSize,
+			TotalPage:   int(helpers.FindTotalPage(count, pagination.PageSize)),
+			PageSize:    pagination.PageSize,
 		},
 	}
 
@@ -72,7 +79,7 @@ func PostParty(c *gin.Context) {
 	x, _ := c.Get("user")
 	user := x.(jwt.MapClaims)
 
-	var body BodyPostParty;
+	var body BodyPostParty
 	c.BindJSON(&body)
 
 	err := initializers.Validate.Struct(body)
@@ -82,22 +89,22 @@ func PostParty(c *gin.Context) {
 		c.Set("error", err)
 		c.Set("error-type", constants.REQUEST_VALIDATION_ERROR)
 		c.Next()
-	} 
+	}
 
 	party := models.Party{
-		Name: body.CompanyName,
+		Name:         body.CompanyName,
 		AddressLine1: body.AddressLine1,
 		AddressLine2: &body.AddressLine2,
 		AddressLine3: &body.AddressLine3,
-		PostalCode: body.PostalCode,
-		CountryId: body.CountryId,
+		PostalCode:   body.PostalCode,
+		CountryId:    body.CountryId,
 	}
 
 	resultParty := db.PSQL.Clauses(clause.Returning{}).Preload("Country").Create(&party)
 
 	permission := models.UserPartyPermission{
-		UserId: user["sub"].(string),
-		PartyId: party.ID,
+		UserId:     user["sub"].(string),
+		PartyId:    party.ID,
 		Permission: "ADMIN",
 	}
 
@@ -105,19 +112,17 @@ func PostParty(c *gin.Context) {
 
 	errorCountryId := "ERROR: insert or update on table \"parties\" violates foreign key constraint \"fk_parties_country\" (SQLSTATE 23503)"
 
-	if (resultParty.Error != nil) {
-		if (resultParty.Error.Error() == errorCountryId) {
+	if resultParty.Error != nil {
+		if resultParty.Error.Error() == errorCountryId {
 			c.Set("error", "Country ID is not found")
 			c.Next()
 		}
 	}
 
-	if (resultPermission.Error != nil) {
+	if resultPermission.Error != nil {
 		fmt.Println(resultPermission.Error.Error())
 	}
 
-
 	c.Set("data", map[string]interface{}{"message": "Successfuly create party", "data": party})
-	
 
 }
--- middlewares/pagination.middleware.go
+++ middlewares/pagination.middleware.go
@@ -8,6 +8,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func generatePageError(msg string) types.ApiError {
+	return types.ApiError{
+		Field: "page",
+		Message: msg,
+	}
+}
+
+func generatePageSizeError(msg string) types.ApiError {
+	return types.ApiError{
+		Field: "page_size",
+		Message: msg,
+	}
+}
+
+
 func PaginationMiddleware(c *gin.Context) {
 	page, pageExist := c.GetQuery("page")
 	pageSize, pageSizeExist := c.GetQuery("page_size")
@@ -25,13 +40,25 @@ func PaginationMiddleware(c *gin.Context) {
 	intPage, err := strconv.Atoi(page);
 	intPageSize, errPageSize := strconv.Atoi(pageSize)
 
+	
+
 	if err != nil {
-		slice := append(errors, types.ApiError{Message: "Page should be number", Field: "page"})
+		slice := append(errors, generatePageError("Page should be number"))
 		errors = slice
 	}
 
 	if errPageSize != nil {
-		slice := append(errors, types.ApiError{Message: "Page Size should be number", Field: "page_size"})
+		slice := append(errors, generatePageSizeError("Page Size should be number"))
+		errors = slice
+	}
+
+	if intPage <= 0 {
+		slice := append(errors, generatePageError("Page should be more than 0"))
+		errors = slice
+	}
+
+	if intPageSize <= 0 {
+		slice := append(errors, generatePageSizeError("Page Size should be more than 0"))
 		errors = slice
 	}
 
@@ -39,6 +66,7 @@ func PaginationMiddleware(c *gin.Context) {
 		c.JSON(400,gin.H{"status": http.StatusBadRequest, "errors": errors})
 		c.Abort()
 	}
+
 
 	offset := (intPage - 1) * intPageSize
 
