01fd69800205677c31490b6205878fe5d0d82bbe
Muhammad Ihsan <ihsan@embreo.global>
2024-01-27 13:51:07 +0700

    feat: ERP-34 add information table

--- models/users.model.go
+++ models/users.model.go
@@ -9,11 +9,12 @@ import (
 
 
 type User struct {
-	Id			string 		`json:"id" gorm:"primaryKey"`
-	Email		string		`json:"email" gorm:"unique"`
-	Password	string		`json:"-"`
-	Phone		*string		`json:"phone" gorm:"unique"`
-	Party		[]Party		`json:"-" gorm:"many2many:user_party_permissions"`
+	Id						string 		`json:"id" gorm:"primaryKey"`
+	Email					string		`json:"email" gorm:"unique"`
+	Password				string		`json:"-"`
+	Phone					*string		`json:"phone" gorm:"unique"`
+	Party					[]Party		`json:"-" gorm:"many2many:user_party_permissions"`
+	InformationChanged		int			`gorm:"default:0"`
 	
 	CreatedAt	time.Time	`json:"-" gorm:"<-:create"`
 	UpdatedAt	*time.Time	`json:"-"`
